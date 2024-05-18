313822934b6e5e71963c8fb961bddb8238453c43
Mager <73451146+Zeronxy@users.noreply.github.com>
2024-05-18 20:37:12 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,528 @@
+const data =
+[
+  {
+    name:'Cleo Tanggung',
+    price:'Rp4000',
+  },
+  {
+    name:'Cleo Kecil',
+    price:'Rp2000',
+  },
+  {
+    name:'Cleo cup',
+    price:'Rp1000',
+  },
+  
+  {
+    name:'Aqua Tanggung',
+    price:'Rp4000',
+  },
+  {
+    name:'Prof Besar',
+    price:'Rp4000',
+  },
+  
+  {
+    name:'Prof Tanggung',
+    price:'Rp4000',
+  },
+  {
+    name:'Prof Kecil',
+    price:'Rp2000',
+  },
+  {
+    name:'Prof Cup',
+    price:'Rp1000',
+  },
+  {
+    name:'Indomilk',
+    price:'Rp3000',
+  },
+  {
+    name:'Milo',
+    price:'Rp3000',
+  },
+  {
+    name:'Milku',
+    price:'Rp5000',
+  },
+  {
+    name:'Ultra Milk',
+    price:'Rp7000',
+  },
+  {
+    name:'Bear Brand',
+    price:'Rp10.000',
+  },
+  {
+    name:'Buavita',
+    price:'Rp7000',
+  },
+  {
+    name:'ABC Kacang Hijau',
+    price:'Rp7000',
+  },
+  
+  {
+    name:'Teh Kotak',
+    price:'Rp5000',
+  },
+  {
+    name:'Teh pucuk',
+    price:'Rp5000',
+  },
+  {
+    name:'ABC Kopi Susu',
+    price:'Rp5000',
+  },
+  {
+    name:'Golda',
+    price:'Rp5000',
+  },
+  {
+    name:'Iso Plus',
+    price:'Rp7000',
+  },
+  {
+    name:'Mizone',
+    price:'Rp7000',
+  },
+  
+  {
+    name:'Sprite Kecil',
+    price:'Rp5000',
+  },
+  {
+    name:'Sprite Tanggung',
+    price:'Rp7000',
+  },
+  {
+    name:'Sprite Kaleng',
+    price:'Rp7000',
+  },
+  {
+    name:'Coca-Cola Kecil',
+    price:'Rp5000',
+  },
+  {
+    name:'Coca-Cola Tanggung',
+    price:'Rp7000',
+  },
+  {
+    name:'Fanta Tanggung',
+    price:'Rp7000',
+  },
+
+  {
+    name:'Fanta Kaleng',
+    price:'Rp5000',
+  },
+  
+  {
+    name:'Power F',
+    price:'Rp1000',
+  },
+  {
+    name:'Mountea',
+    price:'Rp1000',
+  },
+  {
+    name:'Twistko',
+    price:'Rp1000',
+  },
+  {
+    name:'Gery Snack Sereal',
+    price:'Rp1000',
+  },
+  {
+    name:'Twistball',
+    price:'Rp1000',
+  },
+  
+  {
+    name:'French Fries',
+    price:'Rp3000',
+  },
+  {
+    name:'French Fries Besar',
+    price:'Rp5000',
+  },
+  
+  {
+    name:'Ami-Yami',
+    price:'Rp3000',
+  },
+  {
+    name:'Taro',
+    price:'Rp3000',
+  },
+  {
+    name:'Roni',
+    price:'Rp3000',
+  },
+  {
+    name:'Tos Tos',
+    price:'Rp3000',
+  },
+  {
+    name:'Choco Chip',
+    price:'Rp3000',
+  },
+  {
+    name:'Tato Tato',
+    price:'Rp3000',
+  },
+  {
+    name:'Ketagi',
+    price:'Rp3000',
+  },
+  {
+    name:'Roma Malkist besar',
+    price:'Rp6000',
+  },
+  {
+    name:'Roma Malkist Abon Besar',
+    price:'Rp7000',
+  },
+  {
+    name:'Gery salut besar',
+    price:'Rp7000',
+  },
+  
+  {
+    name:'Gabin',
+    price:'Rp6000',
+  },
+  {
+    name:'UBM Biscuit',
+    price:'Rp6000',
+  },
+  {
+    name:'Rosaria',
+    price:'Rp8000',
+  },
+  {
+    name:'Square Puff',
+    price:'Rp12.000',
+  },
+  {
+    name:'Roma Biscuit Kelapa',
+    price:'Rp10.000',
+  },
+  
+  {
+    name:'Roti Aoka',
+    price:'Rp3000',
+  },
+  {
+    name:'Roti Coy',
+    price:'Rp3000',
+  },
+  {
+    name:'Pop Mie',
+    price:'Rp5000',
+  },
+  {
+    name:'Mie',
+    price:'Rp3000',
+  },
+  {
+    name:'Mie Sakura',
+    price:'Rp2000',
+  },
+  
+  {
+    name:'Dayaki',
+    price:'R2000',
+  },
+  {
+    name:'Choki Stix',
+    price:'Rp3000',
+  },
+  {
+    name:'Tango',
+    price:'Rp1000',
+  },
+
+  {
+    name:'Roma Kelapa Cream',
+    price:'Rp3000',
+  },
+  
+  {
+    name:'Qtela',
+    price:'Rp6000',
+  },
+  {
+    name:'Meno Chocolate',
+    price:'Rp1000',
+  },
+  {
+    name:'Good Time',
+    price:'Rp3000',
+  },
+  {
+    name:'Oreo',
+    price:'Rp3000',
+  },
+  {
+    name:'Super Star',
+    price:'Rp3000',
+  },
+  {
+    name:'Chocolatos',
+    price:'Rp1000',
+  },
+  {
+    name:'Cho Cho',
+    price:'Rp2000',
+  },
+  {
+    name:'Go Potato',
+    price:'Rp1000',
+  },
+  {
+    name:'Qtela',
+    price:'Rp6000',
+  },
+  {
+    name:'Makaroni',
+    price:'Rp1000',
+  },
+
+  
+  
+
+  {
+    name:'Tepung Ketan',
+    price:'Rp11.000',
+  },
+  {
+    name:'Tepung Terigu',
+    price:'Rp10.000',
+  },
+  {
+    name:'Tepung Segitiga Biru',
+    price:'Rp14.000',
+  },
+  {
+    name:'Gula',
+    price:'Rp19.000',
+  },
+  
+  {
+    name:'Minyakita 1L',
+    price:'17.000',
+  },
+  {
+    name:'Minyakita 2L',
+    price:'Rp34.000',
+  },
+  
+  {
+    name:'Minyakita 5L',
+    price:'85.000',
+  },
+  {
+    name:'Minyak Sabrina',
+    price:'16.000',
+  },
+  {
+    name:'Kecap Nasional Sedang',
+    price:'Rp16.000',
+  },
+  {
+    name:'Kecap Nasional Kecil',
+    price:'Rp8000',
+  },
+  {
+    name:'Kecap Nasional Botol',
+    price:'Rp6000',
+  },
+  {
+    name:'Sambal Botol',
+    price:'Rp5000',
+  },
+  {
+    name:'Sambal Saset',
+    price:'Rp5000',
+  },
+  {
+    name:'Garam',
+    price:'Rp5000',
+  },
+  {
+    name:'Aji-No-Moto',
+    price:'Rp6000',
+  },
+  {
+    name:'Masako/2pcs(1000)',
+    price:'Rp5000',
+  },
+  {
+    name:'LadaKu',
+    price:'Rp1000',
+  },
+  
+  {
+    name:'Royko Besar',
+    price:'Rp10.000',
+  },
+  {
+    name:'Royko Saset/2pcs(1000)',
+    price:'Rp5000',
+  },
+  {
+    name:'Santan/1pcs(3000)',
+    price:'Rp10.000',
+  },
+  {
+    name:'Agar-Agar Swallow',
+    price:'Rp5000',
+  },
+  {
+    name:'Sajiku Serba Guna',
+    price:'Rp6000',
+  },
+  
+  {
+    name:'Sajiku Bumbu',
+    price:'Rp2000',
+  },
+  {
+    name:'Bumbu Racik',
+    price:'Rp2000',
+  },
+  {
+    name:'Sarden',
+    price:'Rp8000',
+  },
+  {
+    name:'Sampo',
+    price:'R12.000',
+  },
+  {
+    name:'Pasta Gigi',
+    price:'Rp5000',
+  },
+  {
+    name:'Sabun Nuvo',
+    price:'Rp4000',
+  },
+
+  {
+    name:'Lilin',
+    price:'Rp2000',
+  },
+  
+  {
+    name:'Vape',
+    price:'Rp5000',
+  },
+  {
+    name:'Sikat Gigi',
+    price:'Rp5000',
+  },
+  {
+    name:'Mama Lemon',
+    price:'Rp10.000',
+  },
+  {
+    name:'Rinso',
+    price:'Rp10.000',
+  },
+  {
+    name:'Daia',
+    price:'Rp5000',
+  },
+  {
+    name:'So Klin',
+    price:'Rp10.000',
+  },
+  {
+    name:'Molto Saset/2pcs(1000)',
+    price:'Rp6000',
+  },
+  {
+    name:'So Klin Softener Saset/2pcs(1000)',
+    price:'Rp6000',
+  },
+  {
+    name:'Kopi Kapal Api Spesial (Besar)',
+    price:'Rp15.000',
+  },
+  {
+    name:'Kopi Kapal Api Spesial (Sedang)',
+    price:'Rp7000',
+  },
+  {
+    name:'Kopi Kapal Api Spesial (Kecil)',
+    price:'Rp4000',
+  },
+  {
+    name:'Kopi Kapal Api Spesial (Mini)',
+    price:'Rp1000',
+  },
+  {
+    name:'Kopi Kapal Api Spesial*Mix(Kecil)',
+    price:'Rp2000',
+  },
+  {
+    name:'Kopi Kapal Api Kopi+Gula (Kecil)',
+    price:'Rp2000',
+  },
+  {
+    name:'ABC Kopi Susu Saset',
+    price:'Rp2000',
+  },
+  {
+    name:'ToraBika Cappuccino',
+    price:'Rp2000',
+  },
+
+  
+  
+];
+const table = document.getElementById('table');
+const inputData = document.getElementById('input');
+inputData.addEventListener('input',
+function(event){
+  const clik = event.target.value.trim();
+  if(clik){
+    table.innerHTML = '';
+    displayResult(clik)
+  }
+})
+function displayResult(clik) {
+  data.forEach(
+    function(item) {
+      const arr = item.name.split('');
+      const arr1 = clik.split('');
+      if (item.name.includes(clik) && arr[0] == arr1[0] ){
+         render(item.name,item.price);
+      }
+      });
+    
+}
+function notFound(){
+  const tr = document.createElement('tr');
+  const td0 = document.createElement('td');
+  const td01 = document.createElement('td');
+  td0.innerText = 'Belum Ada';
+  td01.innerText = 'Rp0';
+  table.appendChild(tr)
+  tr.appendChild(td0);
+  tr.appendChild(td01);
+}
+function render(name,price){
+  const tr = document.createElement('tr');
+  const td0 = document.createElement('td');
+  const td01 = document.createElement('td');
+  td0.innerText = name;
+  td01.innerText = price;
+  table.appendChild(tr)
+  tr.appendChild(td0);
+  tr.appendChild(td01);
+}
+
+
